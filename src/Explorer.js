ce277d55158afed321314b5b13a42695e74feaaf
kendo <kendo@flipay.co>
2021-03-27 16:27:13 +0700

    can't add image

--- src/Explorer.js
+++ src/Explorer.js
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 import "./App.css";
-import { Heading, Box, Grommet, DataTable, Text, Meter, Button } from "grommet";
+import {
+  Heading,
+  Box,
+  Grommet,
+  DataTable,
+  Text,
+  Meter,
+  Button,
+  Image,
+} from "grommet";
 import Badge from "./Badge";
 import { Link } from "@reach/router";
 
@@ -10,6 +19,11 @@ const Page = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+`;
+
+const Img = styled.img`
+  width: 100px;
+  height: 20px;
 `;
 
 const Padding = styled.div`
@@ -45,6 +59,10 @@ function Explorer() {
               primary: true,
             },
             {
+              property: "price",
+              header: <Text>Price</Text>,
+            },
+            {
               property: "marketCap",
               header: <Text>Market Cap</Text>,
             },
@@ -59,15 +77,11 @@ function Explorer() {
               render: (datum) => <Badge value={datum.weekChange} />,
             },
             {
-              property: "percent",
-              header: "Complete",
+              property: "last7days",
+              header: "Last 7 Days",
               render: (datum) => (
                 <Box pad={{ vertical: "xsmall" }}>
-                  <Meter
-                    values={[{ value: datum.percent }]}
-                    thickness="small"
-                    size="small"
-                  />
+                  <Image src={`./img/${datum.img}.png`} />
                 </Box>
               ),
             },
@@ -86,31 +100,39 @@ function Explorer() {
           data={[
             {
               name: "ABC Fund",
+              price: "$10.5",
               marketCap: "$200,000",
               dayChange: 6,
               weekChange: 24,
               percent: 20,
+              img: "aa",
             },
             {
               name: "XYZ Fund",
+              price: "$30.5",
               marketCap: "$200,000",
               dayChange: -8,
               weekChange: -24,
               percent: 30,
+              img: "bb",
             },
             {
               name: "Big Cap Fund",
+              price: "$40.5",
               marketCap: "$200,000",
               dayChange: 6,
               weekChange: 4,
               percent: 40,
+              img: "cc",
             },
             {
               name: "Defi Fund",
+              price: "$50.5",
               marketCap: "$200,000",
               dayChange: 6,
               weekChange: -4,
               percent: 80,
+              img: "dd",
             },
           ]}
         />
